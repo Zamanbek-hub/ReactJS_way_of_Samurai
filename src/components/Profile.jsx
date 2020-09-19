a5d63c05367917c9a20c3c0458d42a4e1f8c87a7
Zamanbek Turukbayev <zturukbayev888@gmail.com>
2020-09-19 13:13:42 +0600

    13 th lesson GIT REPO

---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://www.lg.com/kz/images/TV/features/D04_TV-UHD-UM71-04-Quad-Core-Processor-Desktop.jpg" />
+      </div>
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>New posts</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
